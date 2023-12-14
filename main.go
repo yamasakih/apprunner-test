65ea413d8a78b2a84dbee387f83ae1d00b07286d
yamasakih <yamasaki.phone@gmail.com>
2023-12-14 22:15:56 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+"fmt"
+"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>Hello, World version1.0.0</h1>")
+}
+
+func main() {
+  http.HandleFunc("/", helloHandler)
+  fmt.Println("Server Start")
+  http.ListenAndServe(":80", nil)
+}
